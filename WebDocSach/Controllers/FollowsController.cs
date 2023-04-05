4f5657d22aedfca419be40c6ed76e123b933e911
Tài Ngọc <hongoctai250402@gmail.com>
2023-04-05 16:22:18 +0700

    cackhoa

---
+++ WebDocSach/Controllers/FollowsController.cs
@@ -0,0 +1,48 @@
+﻿//using Microsoft.AspNet.Identity;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Security.Permissions;
+//using System.Web.Http;
+//using WebDocSach.Models;
+//using WebDocSach.FLOB;
+//using Microsoft.AspNet.Identity.Owin;
+
+
+//namespace WebDocSach.Controllers
+//{
+//    public class FollowsController : ApiController
+//    {
+//        private ApplicationDbContext _dbContext;
+//        public FollowsController()
+//        {
+//            _dbContext = new ApplicationDbContext();
+//        }
+//        [Authorize]
+//        [HttpPost]
+//        public IHttpActionResult Follow(FollowBook followBook)
+//        {
+//            var userId = User.Identity.GetUserId();
+//            var follow = new Follow
+//            {
+//                IdBook = followBook.IdBook,
+//                FolloweeId = userId
+//            };
+//            Follow find = _dbContext.Follows.FirstOrDefault(f => f.FolloweeId == userId && f.IdBook == followBook.IdBook);
+//            if (find == null)
+//            {
+//                _dbContext.Follows.Add(follow);
+
+//            }
+//            else
+//            {
+//                _dbContext.Follows.Remove(find);
+//            }
+
+//            _dbContext.SaveChanges();
+//            return Ok();
+//        }
+//    }
+//}
---
+++ WebDocSach/Models/Follow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebDocSach.Models
+{
+    public class Follow
+    {
+        public int Id { get; set; }
+        public Book Book { get; set; }
+        [Required]
+        public int IdBook { get; set; }
+        public ApplicationUser Followee { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string FolloweeId { get; set; }
+    }
+}
